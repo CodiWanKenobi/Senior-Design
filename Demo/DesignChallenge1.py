0e387001f433838e102244bc81f235fad062ec16
cwmilch <cartermilch@gmail.com>
2020-10-21 12:27:08 -0500

    Code cleanup for console output

--- Demo/DesignChallenge1.py
+++ Demo/DesignChallenge1.py
@@ -10,27 +10,61 @@ class Colors:
     END = '\033[0m'
 
 
+def print_array(arr, description):
+    print(description, end='[')
+    for i in range(len(arr)):
+        delimiter = ', '
+        if i == len(arr) - 1:
+            delimiter = ''
+        print("%d" % arr[i], end=delimiter)
+    print(']')
+
+
+def print_ecc_array(arr, parity_bits, description, diff_index=0, data_only=False,
+                    highlight_parity=True, highlight_diff=True, error=True):
+    print(description, end='[')
+    diff_color = Colors.GREEN
+    if highlight_diff and error:
+        diff_color = Colors.RED
+    for i in range(len(arr)):
+        if data_only and (i & (i - 1)) == 0:
+            continue
+        delimiter = ', '
+        if i == len(arr) - 1:
+            delimiter = ''
+        if highlight_diff and diff_index == i:
+            diff_val = arr[i]
+            if not error:
+                diff_val ^= 1
+            print((diff_color + '%d' + Colors.END) % diff_val, end=delimiter)
+        elif highlight_parity and (parity_bits.__contains__(i) or i == 0):
+            print((Colors.BOLD + '%d' + Colors.END) % arr[i], end=delimiter)  # print parity bits in bold
+        else:
+            print("%d" % arr[i], end=delimiter)
+    print(']')
+
+
 file = input("Enter file name: ")
 with open(file) as f:
-    w, h = [int(x) for x in next(f).split()] # read first line
+    w, h = [int(x) for x in next(f).split()]  # read first line
     array = []
     for line in f:  # read rest of lines
         array.append([int(x) for x in line.split()])
 numECC = math.floor(math.log((w*h), 2)) + 1
 x = np.array(array)
 x = x.flatten()
-print("Original data: ", x)  # data array
+print_array(x, "Original data:\t\t\t\t\t     ")  # data array
 
-y = [0]
-j = 0
-for i in range(1, (len(x)) + numECC + 1):
+y = []
+x_index = 0
+for i in range(len(x) + numECC + 1):
     if (i & (i - 1)) == 0:  # check if power of 2
         y.append(0)
     else:
-        if j < len(x):
-            a = copy.copy(x[j])
+        if x_index < len(x):
+            a = copy.copy(x[x_index])
             y.append(a)
-            j += 1
+            x_index += 1
 
 # Calculate parity bit values for original data
 parityIndices = []
@@ -42,22 +76,13 @@ for i in range(numECC):  # we want to set numECC bits
         if (j & (j-1)) == 0:
             continue
         if i == 0:
-            parityZeroVal ^= y[j]
+            parityZeroVal ^= y[j]  # Need to use all of the data bits for y[0], just do it on the first pass
         if (j & parityIndex) == parityIndex:
             y[parityIndex] ^= y[j]
     parityZeroVal ^= y[parityIndex]
-
 y[0] = parityZeroVal
-print("Data with ECC (parity bits in bold): [", end='')
-for i in range(len(y)):
-    if parityIndices.__contains__(i) or i == 0:
-        print((Colors.BOLD + '%d' + Colors.END + ', ') % y[i], end='')  # print parity bits in bold
-    else:
-        delimiter = ", "
-        if i == len(y) - 1:
-            delimiter = ''
-        print("%d" % y[i], end=delimiter)
-print("]")
+
+print_ecc_array(y, parityIndices, 'Data with ECC (parity bits in bold): ', highlight_diff=False)
 
 errorPos = input("Enter index of bit(s) to flip, separated by a space: ")
 errorIndices = errorPos.split(' ')
@@ -85,59 +110,27 @@ for i in range(numECC):
         errorIndex += (1 << i)
 
 multipleErrors = False
+multiErrorString = 'At least two errors detected - correction not possible'
 if (errorIndex & (errorIndex - 1)) != 0:
     if parityZeroVal == y[0]:
         multipleErrors = True
-        print("At least two errors detected - correction not possible")
+        print(multiErrorString)
     else:
         print("Error detected at position %d" % errorIndex)
 elif errorIndex != 0:
-    print("Error detected in parity bit at position %d" % errorIndex)
+    if parityZeroVal == y[0]:
+        multipleErrors = True
+        print(multiErrorString)
+    else:
+        print("Error detected in parity bit at position %d" % errorIndex)
 else:
     print("No errors detected")
 
 if not multipleErrors:
-    print("Received ECC data:  [", end='')
-    for i in range(len(y)):
-        if errorIndex == i:
-            print((Colors.RED + '%d' + Colors.END + ', ') % y[i], end='')
-        elif parityIndices.__contains__(i) or i == 0:
-            print((Colors.BOLD + '%d' + Colors.END + ', ') % y[i], end='')  # print parity bits in bold
-        else:
-            delimiter = ", "
-            if i == len(y) - 1:
-                delimiter = ''
-            print("%d" % y[i], end=delimiter)
-    print("]")
+    print_ecc_array(y, parityIndices, 'Received ECC data:  ', diff_index=errorIndex)
 
-    print("Corrected ECC data: [", end='')
-    for i in range(len(y)):
-        if errorIndex == i:
-            print((Colors.GREEN + '%d' + Colors.END + ', ') % (y[i] ^ 1), end='')  # print corrected bit in green
-        elif parityIndices.__contains__(i):
-            print((Colors.BOLD + '%d' + Colors.END + ', ') % y[i], end='')  # print parity bits in bold
-        else:
-            delimiter = ", "
-            if i == len(y) - 1:
-                delimiter = ''
-            print("%d" % y[i], end=delimiter)
-    print("]")
+    print_ecc_array(y, parityIndices, 'Corrected ECC data: ', diff_index=errorIndex, error=False)
+    print()
+    print_ecc_array(y, None, 'Corrected data: ', highlight_parity=False, data_only=True)
 
-    print("Corrected data: [", end='')
-    for i in range(len(y)):
-        if (i & (i - 1)) == 0:
-            continue
-        else:
-            delimiter = ", "
-            if i == len(y) - 1:
-                delimiter = ''
-            print("%d" % y[i], end=delimiter)
-    print("]")
-
-    print("Original data:  [", end='')
-    for i in range(len(x)):
-        delimiter = ", "
-        if i == len(x) - 1:
-            delimiter = ''
-        print("%d" % x[i], end=delimiter)
-    print("]")
+    print_array(x, 'Original data:  ')
