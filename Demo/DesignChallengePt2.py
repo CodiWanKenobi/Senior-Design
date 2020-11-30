7168cd6a5106cbd4bce8242ee26fcf33a0610389
cwmilch <cartermilch@gmail.com>
2020-11-30 13:30:38 -0600

    Fix part 2

--- Demo/DesignChallengePt2.py
+++ Demo/DesignChallengePt2.py
@@ -2,7 +2,6 @@ import math
 import random
 import copy
 
-
 class Colors:
     BOLD = '\033[1m'
     GREEN = '\033[92m'
@@ -10,141 +9,169 @@ class Colors:
     END = '\033[0m'
 
 
-def print_array(arr, description):
-    print(description, end='[')
-    for i in range(len(arr)):
-        delimiter = ', '
-        if i == len(arr) - 1:
-            delimiter = ''
-        print("%d" % arr[i], end=delimiter)
-    print(']')
-
-
-def print_ecc_array(arr, parity_bits, description, diff_index=0, data_only=False,
-                    highlight_parity=True, highlight_diff=True, error=True):
-    print(description, end='[')
-    diff_color = Colors.GREEN
-    if highlight_diff and error:
-        diff_color = Colors.RED
-    for i in range(len(arr)):
-        if data_only and (i & (i - 1)) == 0:
-            continue
-        delimiter = ', '
-        if i == len(arr) - 1:
-            delimiter = ''
-        if highlight_diff and diff_index == i:
-            diff_val = arr[i]
-            if not error:
-                diff_val ^= 1
-            print((diff_color + '%d' + Colors.END) % diff_val, end=delimiter)
-        elif highlight_parity and (parity_bits.__contains__(i) or i == 0):
-            print((Colors.BOLD + '%d' + Colors.END) % arr[i], end=delimiter)  # print parity bits in bold
-        else:
-            print("%d" % arr[i], end=delimiter)
-    print(']')
-
-
-numBits = input("Enter the number of data bits (64, 128, or 256): ")
-numBits = int(numBits)
+numBits = 128
+bitsPerBlock = numBits >> 2
 numECC = math.floor(math.log(numBits, 2)) + 1
+generalParityBits = [0] * numECC
+blockParityBits = [0] * 4
 
 x = []
 for i in range(numBits):
     x.append(random.randint(0, 1))
 
-print_array(x, "Original data:\t\t\t\t\t       ")  # data array
+blocks = [x[:32], x[32:64], x[64:96], x[96:128]]
 
-y = []
-x_index = 0
-for i in range(len(x) + numECC + 1):
-    if (i & (i - 1)) == 0:  # check if power of 2
-        y.append(0)
-    else:
-        if x_index < len(x):
-            a = copy.copy(x[x_index])
-            y.append(a)
-            x_index += 1
+# Binary representation of the ECC bits that each group counts for (see Excel sheet on Google Drive).
+# For example, group 0 (groups[0]) counts for ECC bits 0, 1, 2, and 3.
+groups = [
+    0b00001111, 0b00010111, 0b00100111, 0b01000111, 0b10000111, 0b00011011, 0b00101011, 0b01001011,
+    0b10001011, 0b00011101, 0b00101101, 0b01001101, 0b10001101, 0b00011110, 0b00101110, 0b01001110,
+    0b10001110, 0b01111000, 0b10111000, 0b00111001, 0b00111010, 0b11100001, 0b11100010, 0b11100100,
+    0b11101000, 0b11000011, 0b11000110, 0b11001100, 0b11011000, 0b11001010, 0b11010100, 0b11110000
+]
 
-# Calculate parity bit values for original data
-parityIndices = []
-parityZeroVal = 0
-for i in range(numECC):  # we want to set numECC bits
-    parityIndex = (1 << i)
-    parityIndices.append(parityIndex)
-    for j in range(1, len(y)):
-        if (j & (j-1)) == 0:
-            continue
-        if i == 0:
-            parityZeroVal ^= y[j]  # Need to use all of the data bits for y[0], just do it on the first pass
-        if (j & parityIndex) == parityIndex:
-            y[parityIndex] ^= y[j]
-    parityZeroVal ^= y[parityIndex]
-y[0] = parityZeroVal
-origECCArr = copy.copy(y)
 
-print_ecc_array(y, parityIndices, 'Data with ECC (%d parity bits in bold): ' % (numECC + 1), highlight_diff=False)
+# Generate ECC bits based on current data.
+def gen_parity_bits():
+    for block_index in range(len(blocks)):
+        for blockBit in blocks[block_index]:
+            blockParityBits[block_index] ^= blockBit
+    for groupNum in range(len(groups)):
+        group = groups[groupNum]
+        for bitNum in range(0, 8, 1):
+            bit = (group >> bitNum) & 1
+            if bit:
+                for blockNum in range(len(blocks)):
+                    block = blocks[blockNum]
+                    block_bit = block[groupNum]
+                    generalParityBits[bitNum] ^= block_bit
 
-errorPos = input("Enter index of bit(s) to flip, separated by a space: ")
-errorIndices = errorPos.split(' ')
-for i in range(len(errorIndices)):
-    errorIndex = int(errorIndices[i])
-    y[errorIndex] ^= 1
 
-# Calculate parity bit values for modified data
-parityZeroVal = 0
-parityValues = [0] * numECC
-for i in range(numECC):
-    parityIndex = (1 << i)
-    for j in range(1, len(y)):
-        if i == 0:
-            parityZeroVal ^= y[j]
-        if (j & (j - 1)) == 0:
-            continue
-        if (j & parityIndex) == parityIndex:
-            parityValues[i] ^= y[j]
+# Generate syndrome based on parity bits for old and new data.
+# Each argument is a tuple of the form (general_parity_bits, block_parity_bits)
+def gen_syndrome(orig_parity, new_parity):
+    orig_general, orig_block = orig_parity
+    new_general, new_block = new_parity
+    syndrome_general = [0] * len(orig_general)
+    syndrome_block = [0] * len(orig_block)
+    for i in range(len(syndrome_general)):
+        syndrome_general[i] = orig_general[i] ^ new_general[i]
+    for i in range(len(syndrome_block)):
+        syndrome_block[i] = orig_block[i] ^ new_block[i]
+    return syndrome_general, syndrome_block
 
-# Determine error position
-errorIndex = 0
-for i in range(numECC):
-    if parityValues[i] != y[1 << i]:
-        errorIndex += (1 << i)
 
-multipleErrors = False
-multiErrorString = 'At least two errors detected - correction not possible'
-if (errorIndex & (errorIndex - 1)) != 0:
-    if parityZeroVal == y[0]:
-        multipleErrors = True
-        print(multiErrorString)
-    else:
-        print("Error detected at position %d" % errorIndex)
-elif errorIndex != 0:
-    if parityZeroVal == y[0]:
-        multipleErrors = True
-        print(multiErrorString)
-    else:
-        print("Error detected in parity bit at position %d" % errorIndex)
-else:
-    print("No errors detected")
+gen_parity_bits()
+originalGenParity = copy.copy(generalParityBits)
+originalBlockParity = copy.copy(blockParityBits)
 
-if not multipleErrors:
-    print('Original ECC data:  [', end='')
-    for i in range(len(origECCArr)):
-        delimiter = ', '
-        if i == len(origECCArr) - 1:
-            delimiter = ''
-        if origECCArr[i] != y[i]:
-            diff_val = origECCArr[i]
-            print((Colors.RED + '%d' + Colors.END) % diff_val, end=delimiter)
-        if (parityIndices.__contains__(i)):
-            print((Colors.BOLD + '%d' + Colors.END) % origECCArr[i], end=delimiter)  # print parity bits in bold
+old_x = copy.copy(x)  # Keep old data to show changes
+
+# Reset parity bits
+generalParityBits = [0] * numECC
+blockParityBits = [0] * 4
+
+errorPos = input("\nEnter index of bit(s) to flip, separated by a space: ")
+if len(errorPos) > 0:
+    errorIndices = errorPos.split(' ')
+    for i in range(len(errorIndices)):
+        errorIndex = int(errorIndices[i])
+        blockIndex = errorIndex // 32
+        bitIndex = errorIndex % 32
+        blocks[blockIndex][bitIndex] ^= 1
+
+gen_parity_bits()
+oldParityBits = (originalGenParity, originalBlockParity)
+newParityBits = (generalParityBits, blockParityBits)
+general_syndrome, block_syndrome = gen_syndrome(oldParityBits, newParityBits)
+
+print('Old data:')
+for i in range(len(blocks)):
+    for j in range(len(blocks[i])):
+        if i == 3 and j == 31:
+            print(old_x[(i*32 + j)], end='')
         else:
-            print("%d" % origECCArr[i], end=delimiter)
-    print(']')
-
-    print_ecc_array(y, parityIndices, 'Received ECC data:  ', diff_index=errorIndex)
-
-    print_ecc_array(y, parityIndices, 'Corrected ECC data: ', diff_index=errorIndex, error=False)
+            print(old_x[(i*32 + j)], end=', ')
     print()
-    print_ecc_array(y, None, 'Corrected data: ', highlight_parity=False, data_only=True)
 
-    print_array(x, 'Original data:  ')
+print("Old parity:", oldParityBits)
+print()
+print('New data:')
+for i in range(len(blocks)):
+    for j in range(len(blocks[i])):
+        if blocks[i][j] != old_x[(i*32 + j)]:
+            print(Colors.RED, end='')
+            print(blocks[i][j], end='')
+            print(Colors.END, end=', ')
+        else:
+            if i == 3 and j == 31:
+                print(blocks[i][j], end='')
+            else:
+                print(blocks[i][j], end=', ')
+    print()
+
+print("New parity:", newParityBits)
+print()
+print("General Syndrome:", general_syndrome)
+print("Group Syndrome:", block_syndrome)
+print()
+
+
+# Find a group whose binary literal corresponds to the bits set in the syndrome.
+def find_error_group(syndrome):
+    # Treat syndrome as binary representation of integer for easier comparison
+    syndrome_literal = 0
+    for i in range(len(syndrome)):
+        syndrome_literal += syndrome[i] << i
+    if syndrome_literal == 0:  # No error
+        return 0
+    for group_index in range(len(groups)):
+        if syndrome_literal == groups[group_index]:
+            return group_index
+    return -1
+
+
+# Find the first block where an error occurred (only need SEC, so don't worry about the rest).
+def find_error_block(syndrome):
+    for i in range(len(syndrome)):
+        if syndrome[i]:
+            return i
+    return -1
+
+
+groupVal = find_error_group(general_syndrome)
+blockVal = find_error_block(block_syndrome)
+
+if groupVal == 0 and blockVal == -1:
+    print("No error detected.")
+elif (groupVal != 0 and blockVal == -1) or (groupVal == -1 and blockVal != -1):
+    print("Double-error detected - skipping correction.", end='\n\n')
+    print('Final data:')
+    for i in range(len(blocks)):
+        for j in range(len(blocks[i])):
+            if blocks[i][j] != old_x[(i * 32 + j)]:
+                print(Colors.RED, end='')
+                print(blocks[i][j], end='')
+                print(Colors.END, end=', ')
+            else:
+                if i == 3 and j == 31:
+                    print(blocks[i][j], end='')
+                else:
+                    print(blocks[i][j], end=', ')
+        print()
+else:
+    print("Error detected in group %d, block %c" % (groupVal, ['A', 'B', 'C', 'D'][blockVal]), end='\n\n')
+    blocks[blockVal][groupVal] ^= 1
+    print('Corrected data:')
+    for i in range(len(blocks)):
+        for j in range(len(blocks[i])):
+            if i == blockVal and j == groupVal:
+                print(Colors.GREEN, end='')
+                print(blocks[i][j], end='')
+                print(Colors.END, end=', ')
+            else:
+                if i == 3 and j == 31:
+                    print(blocks[i][j], end='')
+                else:
+                    print(blocks[i][j], end=', ')
+        print()
